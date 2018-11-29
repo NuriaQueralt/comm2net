727eb842743ad3db8a6575b3090b22a3543b5973
Aaron Gary <agary@ucsd.edu>
2018-11-29 14:05:29 -0800

    Update readme.md and ui

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # comm2net
+
+To start the web server and rest services run api.py
+
+`python api.py <port number>`
+
+then open a web browser and navigate to
+
+`http://localhost:<port number>`
+
+
--- static/main.js
+++ static/main.js
@@ -1,13 +1,27 @@
+function markType() {
+    alert("made it");
+}
+
+
 function confirmFileSubmit(){
     var input  = document.getElementById('fileToUpload'); // get the input
     var file   = input.files[0];                  // assuming single file, no multiple
     var reader = new FileReader();
 
     reader.onload = function(e) {
+
         var text = reader.result;                 // the entire file
 
         var firstLine = text.split('\n').shift(); // first line
         //var columns = firstLine.split("\t");
+        var countTabs = (firstLine.match(/\t/g) || []).length;
+        var countCommas = (firstLine.match(/,/g) || []).length;
+
+
+        console.log(countTabs);
+        console.log(countCommas);
+
+        if(countTabs)
         var columns = firstLine.split(",");
 
         var previewTableDiv = document.getElementById('previewTableDiv'); // get the input
@@ -16,7 +30,7 @@ function confirmFileSubmit(){
 
         var tbl = document.createElement('table');
             tbl.className = "table table-striped";
-            tbl.style.width = '100%';
+            //tbl.style.width = '100%';
 
             var thead = document.createElement('thead');
 
@@ -66,6 +80,184 @@ function confirmFileSubmit(){
 
     reader.readAsText(file, 'UTF-8');             // or whatever encoding you're using
                                                   // UTF-8 is default, so this argument
+}
+
+function buildPreview(){
+    var input  = document.getElementById('fileToUpload'); // get the input
+    var edgeWell = document.getElementById('edgeWell'); // get the input
+    edgeWell.className = "yesDisplay";
+    var edgeSelect = document.getElementById('edgeSelect'); // get the input
+    edgeSelect.className = "noDisplay";
+
+    var file   = input.files[0];                  // assuming single file, no multiple
+    var reader = new FileReader();
+
+
+
+
+    reader.onload = function(e) {
+
+        var text = reader.result;                 // the entire file
+        var allLines = text.split('\n')
+        var firstLine = allLines.shift(); // first line
+        var secondLine = allLines.shift(); // first line
+        var thirdLine = allLines.shift(); // first line
+
+        console.log(secondLine);
+        console.log(thirdLine);
+
+        //var columns = firstLine.split("\t");
+        var countTabs = (firstLine.match(/\t/g) || []).length;
+        var countCommas = (firstLine.match(/,/g) || []).length;
+
+
+        console.log(countTabs);
+        console.log(countCommas);
+
+        var columns = null;
+        var secondLineFields = null;
+        var thirdLineFields = null;
+        if(countCommas > countTabs) {
+            columns = firstLine.split(",");
+            secondLineFields = secondLine.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+            thirdLineFields = thirdLine.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+        } else {
+            columns = firstLine.split("\t");
+            secondLineFields = secondLine.split("\t");
+            thirdLineFields = thirdLine.split("\t");
+        }
+
+        var previewTableDiv = document.getElementById('previewTableDiv'); // get the input
+
+
+
+        var tbl = document.createElement('table');
+            tbl.className = "table table-bordered table-condensed";
+            //tbl.style.width = '100%';
+
+            //===============
+            // TABLE HEADER
+            //===============
+            var thead = document.createElement('thead');
+            var tr = document.createElement('tr');
+            tr.className = "info";
+
+            var thBlank = document.createElement('th');
+            tr.appendChild(thBlank)
+
+            for (var i=0; i<columns.length; i++) {
+                var th = document.createElement('th');
+                th.className = "text-center";
+                th.innerHTML = "<span style='font-weight: bold;'>" + columns[i] + "</span>";
+                tr.appendChild(th)
+            }
+
+            thead.appendChild(tr);
+
+            tbl.appendChild(thead);
+
+            var tbdy = document.createElement('tbody');
+
+            //=======================
+            // SOURCE RADIO BUTTONS
+            //=======================
+            tbdy.appendChild(createRadioRow("Source", "sourceType", columns))
+
+            //=======================
+            // TARGET RADIO BUTTONS
+            //=======================
+            tbdy.appendChild(createRadioRow("Target", "targetType", columns))
+
+            //============================
+            // INTERACTION RADIO BUTTONS
+            //============================
+            tbdy.appendChild(createRadioRow("Interaction", "interactionType", columns))
+
+            //====================
+            // FIRST LINE PREVIEW
+            //====================
+            tbdy.appendChild(createPreviewRow(secondLineFields));
+
+            //=====================
+            // SECOND LINE PREVIEW
+            //=====================
+            tbdy.appendChild(createPreviewRow(thirdLineFields));
+
+
+            tbl.appendChild(tbdy);
+            previewTableDiv.appendChild(tbl)
+    }
+
+    reader.readAsText(file, 'UTF-8');             // or whatever encoding you're using
+                                                  // UTF-8 is default, so this argument
+}
+
+function createRadioRow(title, type, fields) {
+    var tr = document.createElement('tr');
+    tr.className = "success";
+    var tdTitle = document.createElement('td');
+    tdTitle.innerHTML = "<div style='width: 120px;'><i class='fa fa-link' aria-hidden='true'></i> " + title + " <i class='fa fa-long-arrow-right' aria-hidden='true'></i></div>";
+    tr.appendChild(tdTitle)
+
+    for (var i=0; i<fields.length; i++) {
+        var td = document.createElement('td');
+        td.className = "text-center";
+        td.id = "td" + type +  i.toString();
+        td.innerHTML = "<input type='radio' class='greenButton' onclick='setRadioBackground(\"" + type + i.toString()
+            + "\", \"" + type + "\", " + fields.length + ")' id='" + type + i.toString() + "' name='" + type + "' value='" + fields[i] +"' />";
+        tr.appendChild(td)
+    }
+
+    return tr;
+}
+
+function setRadioBackground(type, group, fieldLength) {
+    var radioTD = document.getElementById("td" + type); // get the input
+    for (var i=0; i<fieldLength; i++) {
+        var td = document.getElementById("td" + group + i.toString());
+        td.style = "";
+    }
+
+    radioTD.style = "background-color: #4DAE4A;";
+}
+
+function createPreviewRow(fields) {
+    console.log(fields);
+    var tr = document.createElement('tr');
+    tr.className = "warning";
+    var tdBlank = document.createElement('td');
+    tr.appendChild(tdBlank)
+
+    for (var i=0; i<fields.length; i++) {
+        var trimmedString = fields[i].length > 20 ? fields[i].substring(0, 20) + "..." : fields[i];
+
+        var td = document.createElement('td');
+        td.className = "text-center";
+        td.innerHTML = trimmedString;
+        tr.appendChild(td)
+    }
+
+    return tr;
+}
+
+function revealSelections() {
+    console.log(getRadioValue("sourceType"));
+    console.log(getRadioValue("targetType"));
+    console.log(getRadioValue("interactionType"));
+    console.log(getRadioValue("nodeID"));
+}
+
+function getRadioValue(radioId) {
+    var sourceRadios = document.getElementsByName(radioId);
+    if(sourceRadios) {
+        for (var i = 0, length = sourceRadios.length; i < length; i++) {
+            if (sourceRadios[i].checked) {
+                return sourceRadios[i].value;
+            }
+        }
+    } else {
+        return null;
+    }
 }
 
 function submitData() {
@@ -135,23 +327,17 @@ function onSuccess(resultsText) {
 
 function checkTable() {
     var returnValue = {};
-    var roleDiv = document.getElementById("previewTableDiv");
-    var roles = roleDiv.getElementsByTagName("select");
-    for(var i=0;i<roles.length; i++){
-        if(roles[i].options[roles[i].selectedIndex].value == "Source Node"){
-            returnValue["Source"] = roles[i].options[roles[i].selectedIndex].attributes["tag"].value;
-        } else if(roles[i].options[roles[i].selectedIndex].value == "Target Node"){
-            returnValue["Target"] = roles[i].options[roles[i].selectedIndex].attributes["tag"].value;
-        } else if(roles[i].options[roles[i].selectedIndex].value == "Edge Interaction"){
-            returnValue["Edge"] = roles[i].options[roles[i].selectedIndex].attributes["tag"].value;
-        }
-    }
 
-    var nodeId = document.getElementById("nodeId");
-    if(nodeId != null) {
-        returnValue["NodeIDColumn"] = nodeId.options[nodeId.selectedIndex].value;
-        console.log(returnValue["NodeIDColumn"]);
-    }
+    returnValue["Source"] = getRadioValue("sourceType");
+    returnValue["Target"] = getRadioValue("targetType");
+    returnValue["Edge"] = getRadioValue("interactionType");
+    returnValue["NodeIDColumn"] = getRadioValue("nodeID");
+
+    //var nodeId = document.getElementById("nodeId");
+    //if(nodeId != null) {
+    //    returnValue["NodeIDColumn"] = nodeId.options[nodeId.selectedIndex].value;
+    //    console.log(returnValue["NodeIDColumn"]);
+    //}
 
     return returnValue;
 }
@@ -165,46 +351,81 @@ function confirmFileSubmitNode(){
 
     reader.onload = function(e) {
         var text = reader.result;                 // the entire file
+        var allLines = text.split('\n')
+        var firstLine = allLines.shift(); // first line
+        var secondLine = allLines.shift(); // first line
+        var thirdLine = allLines.shift(); // first line
 
-        var firstLine = text.split('\n').shift(); // first line
+        console.log(secondLine);
+        console.log(thirdLine);
+
         //var columns = firstLine.split("\t");
-        var columns = firstLine.split(",");
+        var countTabs = (firstLine.match(/\t/g) || []).length;
+        var countCommas = (firstLine.match(/,/g) || []).length;
+
+        var columns = null;
+        var secondLineFields = null;
+        var thirdLineFields = null;
+        if(countCommas > countTabs) {
+            columns = firstLine.split(",");
+            secondLineFields = secondLine.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+            thirdLineFields = thirdLine.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+        } else {
+            columns = firstLine.split("\t");
+            secondLineFields = secondLine.split("\t");
+            thirdLineFields = thirdLine.split("\t");
+        }
 
         var previewTableDiv = document.getElementById('previewTableDivNode'); // get the input
 
+
+
+
         var tbl = document.createElement('table');
-            tbl.className = "table table-striped";
-            tbl.style.width = '100%';
+        tbl.className = "table table-bordered table-condensed";
+        //tbl.style.width = '100%';
 
-            var thead = document.createElement('thead');
+        //===============
+        // TABLE HEADER
+        //===============
+        var thead = document.createElement('thead');
+        var tr = document.createElement('tr');
+        tr.className = "info";
 
-            var tr = document.createElement('tr');
+        var thBlank = document.createElement('th');
+        tr.appendChild(thBlank)
 
-            var td = document.createElement('td');
-            td.style.width = "150"
-            td.appendChild(document.createTextNode("Select Node ID"))
-            tr.appendChild(td)
-            thead.appendChild(tr);
+        for (var i=0; i<columns.length; i++) {
+            var th = document.createElement('th');
+            th.className = "text-center";
+            th.innerHTML = "<span style='font-weight: bold;'>" + columns[i] + "</span>";
+            tr.appendChild(th)
+        }
 
-            tbl.appendChild(thead);
+        thead.appendChild(tr);
+
+        tbl.appendChild(thead);
+
+        var tbdy = document.createElement('tbody');
+
+        //=======================
+        // SOURCE RADIO BUTTONS
+        //=======================
+        tbdy.appendChild(createRadioRow("Node ID", "nodeID", columns))
+
+        //====================
+        // FIRST LINE PREVIEW
+        //====================
+        tbdy.appendChild(createPreviewRow(secondLineFields));
+
+        //=====================
+        // SECOND LINE PREVIEW
+        //=====================
+        tbdy.appendChild(createPreviewRow(thirdLineFields));
 
 
-            var tbdy = document.createElement('tbody');
-            var tr = document.createElement('tr');
-            var td = document.createElement('td');
-            var nodeSelectText = "<select id='nodeId'  >\n" +
-                "  <option>--Select node id column--</option>\n";
-            for (var i = 0; i < columns.length; i++) {
-                nodeSelectText += "  <option tag='" + columns[i] + "'>" + columns[i] + "</option>\n";
-            }
-
-            nodeSelectText += "</select>";
-            td.innerHTML = nodeSelectText;
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-            tbl.appendChild(tbdy);
-            previewTableDiv.appendChild(tbl)
-        console.log(firstLine);                   // use the console for debugging
+        tbl.appendChild(tbdy);
+        previewTableDiv.appendChild(tbl);
     }
 
     reader.readAsText(file, 'UTF-8');             // or whatever encoding you're using
